4fc6fe22aab2bd52189cd213b62f4a94b544c2d4
Minhsun88 <4a7k0032@stust.edu.tw>
2022-07-12 15:45:56 +0800

    feat:簡易Submit事件

--- js training/message.js
+++ js training/message.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-  $(document).on('submit', 'submitform', function() {
-    // $("#alert").fadeToggle(slow);
-    console.log('123213');
+$(document).ready(function () {
+  $(document).on('submit', '#submitform', function () {
+    $('#alert').text('註冊成功').show().fadeOut(1000);
   });
 })
-
